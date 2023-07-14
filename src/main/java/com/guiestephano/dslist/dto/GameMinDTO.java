7de3706a40c29e33bd9b53a99b0f7f916d68be54
Guilherme Estephano Marques Alves <guiestephano@gmail.com>
2023-07-13 22:55:22 -0300

    Move games on list

--- src/main/java/com/guiestephano/dslist/dto/GameMinDTO.java
+++ src/main/java/com/guiestephano/dslist/dto/GameMinDTO.java
@@ -1,6 +1,7 @@
 package com.guiestephano.dslist.dto;
 
 import com.guiestephano.dslist.entities.Game;
+import com.guiestephano.dslist.projections.GameMinProjection;
 
 public class GameMinDTO {
 	private Long id;
@@ -17,6 +18,14 @@ public class GameMinDTO {
 		year = entity.getYear();
 		imgUrl = entity.getImgUrl();
 		shortDescription = entity.getShortDescription();
+	}
+	
+	public GameMinDTO(GameMinProjection projection) {
+		id = projection.getId();
+		title = projection.getTitle();
+		year = projection.getYear();
+		imgUrl = projection.getImgUrl();
+		shortDescription = projection.getShortDescription();
 	}
 
 	public Long getId() {
